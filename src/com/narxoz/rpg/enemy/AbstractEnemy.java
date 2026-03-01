c12f4387d1ab5aa8f50d733799abd9c9e9a5531d
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-03-01 19:20:47 +0500

    Fix AbstractEnemy clone method and remove compile errors

--- src/com/narxoz/rpg/enemy/AbstractEnemy.java
+++ src/com/narxoz/rpg/enemy/AbstractEnemy.java
@@ -5,93 +5,78 @@ import com.narxoz.rpg.loot.LootTable;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class AbstractEnemy implements Enemy {
+public abstract class AbstractEnemy implements Enemy, Cloneable {
     protected String name;
-    public int health;
-    public int damage;
-    public int defense;
-    public int speed;
-    public String element;
-    public List<Ability> abilities;
-    public LootTable lootTable;
-    public String aiBehavior;
-    
+    protected int health;
+    protected int damage;
+    protected int defense;
+    protected int speed;
+    protected String element;
+    protected List<Ability> abilities;
+    protected LootTable lootTable;
+    protected String aiBehavior;
+
     public AbstractEnemy(String name) {
         this.name = name;
         this.abilities = new ArrayList<>();
         this.element = "NONE";
     }
-    
+
+   
     @Override
-    public String getName() {
-        return name;
-    }
-    
+    public String getName() { return name; }
+
     @Override
-    public int getHealth() {
-        return health;
-    }
-    
+    public int getHealth() { return health; }
+
     @Override
-    public int getDamage() {
-        return damage;
-    }
-    
+    public int getDamage() { return damage; }
+
     @Override
-    public int getDefense() {
-        return defense;
-    }
-    
+    public int getDefense() { return defense; }
+
     @Override
-    public int getSpeed() {
-        return speed;
-    }
-    
+    public int getSpeed() { return speed; }
+
     @Override
-    public String getElement() {
-        return element;
-    }
-    
+    public String getElement() { return element; }
+
     @Override
-    public List<Ability> getAbilities() {
-        return abilities;
-    }
-    
+    public List<Ability> getAbilities() { return abilities; }
+
     @Override
-    public void addAbility(Ability ability) {
-        abilities.add(ability);
-    }
-    
+    public void addAbility(Ability ability) { abilities.add(ability); }
+
     @Override
-    public LootTable getLootTable() {
-        return lootTable;
-    }
-    
+    public LootTable getLootTable() { return lootTable; }
+
     @Override
-    public void setLootTable(LootTable lootTable) {
-        this.lootTable = lootTable;
-    }
-    
+    public void setLootTable(LootTable lootTable) { this.lootTable = lootTable; }
+
     @Override
-    public String getAIBehavior() {
-        return aiBehavior;
-    }
-    
+    public String getAIBehavior() { return aiBehavior; }
+
     @Override
-    public void setAIBehavior(String aiBehavior) {
-        this.aiBehavior = aiBehavior;
-    }
-    
+    public void setAIBehavior(String aiBehavior) { this.aiBehavior = aiBehavior; }
+
     @Override
-    public void setElement(String element) {
-        this.element = element;
-    }
-    
+    public void setElement(String element) { this.element = element; }
+
     @Override
     public void multiplyStats(double multiplier) {
         this.health = (int)(this.health * multiplier);
         this.damage = (int)(this.damage * multiplier);
         this.defense = (int)(this.defense * multiplier);
         this.speed = (int)(this.speed * multiplier);
+    }
+
+    
+    @Override
+    public AbstractEnemy clone() {
+        try {
+            return (AbstractEnemy) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Cloning not supported", e);
+        }
     }
 }
